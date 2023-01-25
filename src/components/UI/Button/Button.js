c6f5d9f6c3823b1f278c246511f66be5fdf2794e
gdtknight <gdtknight@naver.com>
2023-01-25 21:49:18 +0900

    #1 - styled-components => css module 방식으로 변경
    css module 방식으로 style을 적용하기 위해선
    
    css파일 확장자를 .css 에서 .module.css 로 바꿔야 한다.
    
    React import 할 때와 비슷하게 `import styles from './xxx.module.css'` 로 import 후
    
    styles.`selector` 와 같은 형식으로 css attribute를 정의하여 style 적용이 가능하다.

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,33 +1,48 @@
-import styled from 'styled-components';
+import React from 'react';
 
-// back-tick을 통해 button method로 인수 전달 가능.
-// button method는 새로운 Button Component를 반환한다.
-const Button = styled.button`
-  width: 100%;
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+// CSS Module 사용
+import styles from './Button.module.css';
 
-  @media (min-width: 768px) {
-    width: auto;
-  }
+// import styled from 'styled-components';
 
-  /* pseudo-selector */
-  /* &: 의 형식으로 사용 */
-  &:focus {
-    outline: none;
-  }
+// // back-tick을 통해 button method로 인수 전달 가능.
+// // button method는 새로운 Button Component를 반환한다.
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
+
+//   /* pseudo-selector */
+//   /* &: 의 형식으로 사용 */
+//   &:focus {
+//     outline: none;
+//   }
+
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
+
+
+// styles.button 은 compiile 후에 Button_button__HashValue 와 같은 형태가 된다.
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
