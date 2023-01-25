13099445d2afbca8f8c189103ad867241e5b279f
gdtknight <gdtknight@naver.com>
2023-01-25 20:57:19 +0900

    #1 - styled-components 적용
    styled-components 기본 문법
    
    ```javascript
    
    import styled from 'styled-components';
    
    const '컴포넌트 이름'= styled.div`
    
      div class에 기본적으로 적용할 속성들
    
     & -> psudo element 혹은 추가적인 selector 선택 위해 자기 자신을 지정할 때
    
    `;
    
    ```
    
    styled-component 에 props 를 적용시켜서 dynamic css style 적용이 가능하다.

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,45 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  /* & -> div 안에 있는 레이블에 대해 얘기하고 있어 ~ */
+  font-weight: bold;
+  & label {
+    display: block;
+    margin-bottom: 0.5rem;
+  color: ${props => (props.invalid ? 'red' : 'black')};
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+    background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  &.invalid input{
+    border-color: red;
+    background: #ffd7d7;
+  }
+
+  &.invalid label {
+    color: red;
+  }
+`;
 
 const CourseInput = props => {
 
@@ -24,12 +62,13 @@ const CourseInput = props => {
     props.onAddGoal(enteredValue);
   };
 
+  // <FormControl className={!isValid && 'invalid'}>
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      <FormControl invalid={(!isValid)}>
         <label >Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import styled from 'styled-components';
 
-import './Button.css';
+// back-tick을 통해 button method로 인수 전달 가능.
+// button method는 새로운 Button Component를 반환한다.
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+  /* pseudo-selector */
+  /* &: 의 형식으로 사용 */
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`;
 
 export default Button;
